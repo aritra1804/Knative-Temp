890281538ce060484c386fab4df5f25a832fb9dc
aritra1804 <aritradutta04@gmail.com>
2024-01-22 09:54:05 +0530

    Autoscaling added

--- infra.tf
+++ infra.tf
@@ -6,20 +6,21 @@ terraform {
         }
     }
 }
-
 provider "linode" {
     token = var.linode_api_token
 }
-
 locals {
     root_dir = "${abspath(path.root)}"
     k8s_config_dir = "${local.root_dir}/.kube/"
     k8s_config_file = "${local.root_dir}/.kube/kubeconfig.yaml"
 }
-
 variable "linode_api_token" {
     description = "Your Linode API Personal Access Token. (required)"
     sensitive   = true
+}
+
+variable "k8s_node_type" {
+  default="g6-standard-2"
 }
 
 resource "linode_lke_cluster" "terraform_k8s" {
@@ -28,8 +29,12 @@ resource "linode_lke_cluster" "terraform_k8s" {
     region="us-east"
     tags=["terraform-k8s"]
     pool {
-        type  = "g6-standard-1"
+        type  = var.k8s_node_type
         count = 3
+        autoscaler {
+            min = 3
+            max = 8
+        }
     }
 }
 
